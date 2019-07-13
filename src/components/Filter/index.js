503d75e6873ffada76fde6d644dbfcb7b3edeedb
Igor Lesnevskiy <lesnevskii.igor@gmail.com>
2019-07-13 09:28:17 +0300

    remove filter hardcode

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,95 +1,112 @@
-import React, {useState, useCallback, useReducer} from "react";
-import {utils} from '../../tools';
+import React, {useCallback, useReducer} from "react";
 import styles from './styles.module.scss';
 import FilterSectionWrapper from "../FilterSectionWrapper";
 
-const data = [
-	{
-		title: "Валюта",
-		type: "currencyChecker",
-		data: [
-			{
-				label: "RUB",
-				value: "RUB",
-				name: "currency",
-				isChecked: true,
-				// id: utils.getUniqId(),
-				id: "unique1",
-			},
-			{
-				label: "USD",
-				value: "USD",
-				name: "currency",
-				isChecked: false,
-				// id: utils.getUniqId(),
-				id: "unique2",
-			},
-			{
-				label: "EUR",
-				value: "EUR",
-				name: "currency",
-				isChecked: false,
-				// id: utils.getUniqId(),
-				id: "unique3",
-			},
-		]
-	},
-	{
-		title: "Количество пересадок",
-		type: "checkboxesList",
-		data: [
-			{
-				only: false,
-				name: "stops",
-				value: "all",
-				label: "Все",
-				// id: utils.getUniqId(),
-				id: "unique4",
-				isChecked: false,
-			},
-			{
-				label: "Без пересадок",
-				name: "stops",
-				value: "0",
-				// id: utils.getUniqId(),
-				id: "unique5",
-				isChecked: false,
-			},
-			{
-				label: "1 пересадка",
-				name: "stops",
-				value: "1",
-				// id: utils.getUniqId(),
-				id: "unique6",
-				isChecked: false,
-			},
-			{
-				label: "2 пересадки",
-				name: "stops",
-				value: "2",
-				// id: utils.getUniqId(),
-				id: "unique7",
-				isChecked: false,
-			},
-			{
-				label: "3 пересадки",
-				name: "stops",
-				value: "3",
-				// id: utils.getUniqId(),
-				id: "unique8",
-				isChecked: false,
-			}
-		]
-	},
-];
+// const data = [
+// 	{
+// 		title: "Валюта",
+// 		type: "currencyChecker",
+// 		data: [
+// 			{
+// 				label: "RUB",
+// 				value: "RUB",
+// 				name: "currency",
+// 				isChecked: false,
+// 				// id: utils.getUniqueId(),
+// 				id: "unique1",
+// 			},
+// 			{
+// 				label: "USD",
+// 				value: "USD",
+// 				name: "currency",
+// 				isChecked: false,
+// 				// id: utils.getUniqueId(),
+// 				id: "unique2",
+// 			},
+// 			{
+// 				label: "EUR",
+// 				value: "EUR",
+// 				name: "currency",
+// 				isChecked: true,
+// 				// id: utils.getUniqueId(),
+// 				id: "unique3",
+// 			},
+// 		]
+// 	},
+// 	{
+// 		title: "Количество пересадок",
+// 		type: "checkboxesList",
+// 		data: [
+// 			{
+// 				only: false,
+// 				name: "stops",
+// 				value: "all",
+// 				label: "Все",
+// 				// id: utils.getUniqueId(),
+// 				id: "unique4",
+// 				isChecked: false,
+// 			},
+// 			{
+// 				label: "Без пересадок",
+// 				name: "stops",
+// 				value: "0",
+// 				// id: utils.getUniqueId(),
+// 				id: "unique5",
+// 				isChecked: false,
+// 			},
+// 			{
+// 				label: "1 пересадка",
+// 				name: "stops",
+// 				value: "1",
+// 				// id: utils.getUniqueId(),
+// 				id: "unique6",
+// 				isChecked: true,
+// 			},
+// 			{
+// 				label: "2 пересадки",
+// 				name: "stops",
+// 				value: "2",
+// 				// id: utils.getUniqueId(),
+// 				id: "unique7",
+// 				isChecked: false,
+// 			},
+// 			{
+// 				label: "3 пересадки",
+// 				name: "stops",
+// 				value: "3",
+// 				// id: utils.getUniqueId(),
+// 				id: "unique8",
+// 				isChecked: false,
+// 			}
+// 		]
+// 	},
+// ];
 
 function Filter(props) {
-	const [formData, setFormData] = useState({});
 	const {
-		onFilterUpdate = Function.prototype
+		onFilterUpdate = Function.prototype,
+		data = []
 	} = props;
 
-	const onSectionUpdate = sectionData => {
+	const [formData, dispatch] = useReducer((state, action) => {
+		switch (action.type) {
+			case "update":
+				{
+					const result = {
+						...state,
+						...action.data
+					};
+
+					onFilterUpdate(result);
+					return result;
+				}
+			default:
+				console.error('Unknown dispatch case');
+				break;
+		}
+	}, {});
+
+	const onSectionUpdate = useCallback(sectionData => {
 		const {
 			type,
 			data
@@ -98,8 +115,6 @@ function Filter(props) {
 		if (!type || !data || !Object.keys(data).length) {
 			return false;
 		}
-
-		let result = {};
 
 		switch (type) {
 			case 'checkboxesList':
@@ -113,23 +128,22 @@ function Filter(props) {
 					return false;
 				}
 
-				result = {
-					...formData,
-					[name]: {
-						type,
-						value: checkedValues.length > 1 ? checkedValues : checkedValues.pop()
+				dispatch({
+					type: "update",
+					data: {
+						[name]: {
+							type,
+							value: checkedValues.length > 1 ? checkedValues : checkedValues.pop()
+						}
 					}
-				};
-
-				setFormData(result);
-				onFilterUpdate(result);
+				});
 
 				break;
 			default:
 				console.error('Unknown filter case');
 				break;
 		}
-	};
+	}, [onFilterUpdate]);
 
 	const sections = data.map((sectionData, key) => {
 		return <React.Fragment key={key}>
