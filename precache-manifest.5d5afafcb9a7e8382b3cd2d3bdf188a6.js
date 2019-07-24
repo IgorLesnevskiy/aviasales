3b7cda1bf88b15ad5b6df311f5c9ee2f1410f752
Igor Lesnevskiy <lesnevskii.igor@gmail.com>
2019-07-24 08:23:24 +0300

    Updates

---
+++ precache-manifest.5d5afafcb9a7e8382b3cd2d3bdf188a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f68e70be4fab6c7aa7d92dad10726e",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "192d410b5ed159c416a2",
+    "url": "/aviasales/static/css/main.4b2a23e0.chunk.css"
+  },
+  {
+    "revision": "31df975d55ce82e722a9",
+    "url": "/aviasales/static/js/2.90027654.chunk.js"
+  },
+  {
+    "revision": "192d410b5ed159c416a2",
+    "url": "/aviasales/static/js/main.0359003e.chunk.js"
+  },
+  {
+    "revision": "af052bb48cec26b9b243",
+    "url": "/aviasales/static/js/runtime~main.f8f776b2.js"
+  },
+  {
+    "revision": "c51caac2e76ea5320c41a80beeed7d4e",
+    "url": "/aviasales/static/media/logo.c51caac2.svg"
+  }
+]);
